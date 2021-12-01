ac1f4503769e71d5fd5aa794b8fc70cad8639343
Matt <mathew.beldon@hotmail.co.uk>
2021-12-01 16:28:00 +0000

    First Day

---
+++ AdventOfCodeDayOne/AdventOfCodeDayOne/Program.cs
@@ -0,0 +1,41 @@
+﻿int[] arr = File.ReadLines(@"C:\src\AOC\AdventOfCodeDayOne\input.txt").Select(x => int.Parse(x)).ToArray();
+
+int partOneResult = PartOne(arr);
+int partTwoResult = PartTwo(arr);
+
+Console.WriteLine($"Part One: {partOneResult} increases");
+Console.WriteLine($"Part Two: {partTwoResult} increases");
+
+int PartOne(int[] arr)
+{
+    int increase = 0;
+
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        if (arr[i] < arr[i + 1])
+        {
+            increase++;
+        }
+    }
+    return increase;
+}
+
+int PartTwo(int[] arr)
+{
+    const int SLIDING_WINDOW_LENGTH = 3;
+    int increase = 0;
+
+    int firstSum = arr[0..SLIDING_WINDOW_LENGTH].Sum();
+    int secondSum = firstSum;
+
+    for (int i = 1; i <= arr.Length - SLIDING_WINDOW_LENGTH; i++)
+    {
+        firstSum = secondSum;
+        secondSum = arr[i..(i + SLIDING_WINDOW_LENGTH)].Sum();
+        if (firstSum < secondSum)
+        {
+            increase++;
+        }
+    }
+    return increase;
+}
